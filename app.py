4e0292ff6a6f94a0bf3352e97bdb87e4a28f6ae1
muskanbansal675 <muskanpreet675@gmail.com>
2025-08-29 12:56:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from sklearn.linear_model import LogisticRegression
 import pandas as pd
-
+import os
 
 # Load data
 url = "iris.csv"
@@ -44,5 +44,6 @@ def predict():
 
 if __name__ == '__main__':
     app.run()
+
 
 
